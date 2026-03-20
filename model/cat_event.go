4481398fd4d381fe8c3ece63219994825ca127e4
NekokeCore <core2002@163.com>
2026-03-21 00:36:18 +0800

    style: 规范字段注释对齐

--- model/cat_event.go
+++ model/cat_event.go
@@ -16,9 +16,9 @@ const (
 type CatEvent struct {
 	ID        uint         `json:"id" gorm:"primaryKey"`
 	CatID     uint         `json:"cat_id" gorm:"not null"`
-	Type      CatEventType `json:"event_type" gorm:"size:100;not null"`
-	Detail    string       `json:"detail" gorm:"size:1000;not null"` // 事件详情
-	EventTime time.Time    `json:"event_time" gorm:"not null"`       // 发生时间
+	Type      CatEventType `json:"event_type" gorm:"size:100;not null"` // 事件项目
+	Detail    string       `json:"detail" gorm:"size:1000;not null"`    // 事件详情
+	EventTime time.Time    `json:"event_time" gorm:"not null"`          // 发生时间
 }
 
 type CatActionType string
@@ -41,8 +41,8 @@ const (
 type CatAction struct {
 	ID     uint          `json:"id" gorm:"primaryKey"`
 	CatID  uint          `json:"cat_id" gorm:"not null"`
-	UserID uint          `json:"user_id" gorm:"not null"` // 执行人
-	Time   time.Time     `json:"time" gorm:"not null"`    // 执行时间
-	Type   CatActionType `json:"type" gorm:"size:100;not null"`
+	UserID uint          `json:"user_id" gorm:"not null"`          // 执行人
+	Time   time.Time     `json:"time" gorm:"not null"`             // 执行时间
+	Type   CatActionType `json:"type" gorm:"size:100;not null"`    // 执行项目
 	Detail string        `json:"detail" gorm:"size:1000;not null"` // 事件详情
 }
