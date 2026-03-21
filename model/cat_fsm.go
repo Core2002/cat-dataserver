e211f40dd8a4e0ce05aa23d7fa79286c39651fc2
NekokeCore <core2002@163.com>
2026-03-21 20:30:09 +0800

    refactor: 修改 CatFSM 字段类型

--- model/cat_fsm.go
+++ model/cat_fsm.go
@@ -4,7 +4,7 @@ import "time"
 
 type CatFSM struct {
 	CatID         uint      `json:"cat_id" gorm:"primaryKey"`
-	InHospital    string    `json:"in_hospital" gorm:"not null"`
+	HospitalID    uint      `json:"hospital_id" gorm:"not null"`
 	TemperatureC  float32   `json:"temperature" gorm:"not null"`
 	WeightKG      float32   `json:"weight_kg" gorm:"not null"`
 	TrimNailsTime time.Time `json:"trim_nails_time" gorm:"not null"`
