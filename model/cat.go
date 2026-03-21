ec046b8a95b968951d688b9c5a2bed5367e19ba2
NekokeCore <core2002@163.com>
2026-03-21 20:46:19 +0800

    refactor: 调整 Cat 结构体字段顺序

--- model/cat.go
+++ model/cat.go
@@ -4,10 +4,10 @@ type Cat struct {
 	CatID             uint   `json:"cat_id" gorm:"primaryKey"`                     // 猫ID
 	CatName           string `json:"cat_name" gorm:"size:100;not null"`            // 猫名
 	CatPhotoUri       string `json:"cat_photo_uri" gorm:"size:1000;not null"`      // 猫照片
-	MasterName        string `json:"master_name" gorm:"size:100;not null"`         // 主人姓名
-	MasterPhoneNumber string `json:"master_phone_number" gorm:"size:100;not null"` // 主人电话
 	CatType           string `json:"cat_type" gorm:"size:100;not null"`            // 猫种类
 	CatGender         string `json:"cat_gender" gorm:"size:100;not null"`          // 猫性别
+	MasterName        string `json:"master_name" gorm:"size:100;not null"`         // 主人姓名
+	MasterPhoneNumber string `json:"master_phone_number" gorm:"size:100;not null"` // 主人电话
 }
 
 type CatProfile struct {
