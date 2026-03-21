021b707455190912003dee69b447b089333bd15d
NekokeCore <core2002@163.com>
2026-03-21 20:32:48 +0800

    fix: 修正JSON字段命名以符合规范

--- model/cat_fsm.go
+++ model/cat_fsm.go
@@ -5,7 +5,7 @@ import "time"
 type CatFSM struct {
 	CatID         uint      `json:"cat_id" gorm:"primaryKey"`
 	HospitalID    uint      `json:"hospital_id" gorm:"not null"`
-	TemperatureC  float32   `json:"temperature" gorm:"not null"`
+	TemperatureC  float32   `json:"temperature_c" gorm:"not null"`
 	WeightKG      float32   `json:"weight_kg" gorm:"not null"`
 	TrimNailsTime time.Time `json:"trim_nails_time" gorm:"not null"`
 }
--- model/site.go
+++ model/site.go
@@ -3,12 +3,12 @@ package model
 import "time"
 
 type Site struct {
-	ID                uint      `json:"id" gorm:"primaryKey"`
-	Name              string    `json:"name" gorm:"size:100;not null"`
-	Address           string    `json:"address" gorm:"size:100;not null"`
-	AdminPhoneNumber  string    `json:"phone_number" gorm:"size:100;not null"`
-	LastDisinfectTime time.Time `json:"disinfect_time" gorm:"size:100;not null"` // 上次消毒时间
-	LastFeedTime      time.Time `json:"feed_time" gorm:"not null"`               // 上次喂食时间
-	LastGiveWaterTime time.Time `json:"give_water_time" gorm:"not null"`         // 上次喂水时间
-	LastPlayTime      time.Time `json:"play_time" gorm:"not null"`               // 上次逗猫时间
+	ID                   uint      `json:"id" gorm:"primaryKey"`
+	SiteName             string    `json:"site_name" gorm:"size:100;not null"`
+	SiteAddress          string    `json:"site_address" gorm:"size:100;not null"`
+	SiteAdminPhoneNumber string    `json:"site_admin_phone_number" gorm:"size:100;not null"`
+	LastDisinfectTime    time.Time `json:"disinfect_time" gorm:"size:100;not null"` // 上次消毒时间
+	LastFeedTime         time.Time `json:"feed_time" gorm:"not null"`               // 上次喂食时间
+	LastGiveWaterTime    time.Time `json:"give_water_time" gorm:"not null"`         // 上次喂水时间
+	LastPlayTime         time.Time `json:"play_time" gorm:"not null"`               // 上次逗猫时间
 }
