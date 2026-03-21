ce027edf6199ed1aed0130f5dd787457249f6aed
NekokeCore <core2002@163.com>
2026-03-21 20:50:33 +0800

    feat: 添加事件地点字段

--- model/cat_event.go
+++ model/cat_event.go
@@ -17,6 +17,7 @@ type CatEvent struct {
 	EventID   uint         `json:"event_id" gorm:"primaryKey"`
 	EventType CatEventType `json:"event_type" gorm:"size:100;not null"` // 事件项目
 	EventTime time.Time    `json:"event_time" gorm:"not null"`          // 发生时间
+	SiteID    uint         `json:"site_id" gorm:"not null"`             // 事件地点
 	CatID     uint         `json:"cat_id" gorm:"not null"`              // 发生事件的猫
 	Detail    string       `json:"detail" gorm:"size:1000;not null"`    // 事件详情
 }
@@ -41,8 +42,9 @@ const (
 type CatAction struct {
 	ActionID     uint          `json:"action_id" gorm:"primaryKey"`
 	CatID        uint          `json:"cat_id" gorm:"not null"`                  // 被执行的猫
-	UserID       uint          `json:"user_id" gorm:"not null"`                 // 执行人
 	ActionTime   time.Time     `json:"action_time" gorm:"not null"`             // 执行时间
+	SiteID       uint          `json:"site_id" gorm:"not null"`                 // 执行地点
+	UserID       uint          `json:"user_id" gorm:"not null"`                 // 执行人
 	ActionType   CatActionType `json:"action_type" gorm:"size:100;not null"`    // 执行项目
 	ActionDetail string        `json:"action_detail" gorm:"size:1000;not null"` // 事件详情
 }
