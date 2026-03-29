81ed770a9437c169f6abec8d0f3a456189cf11f9
NekokeCore <core2002@163.com>
2026-03-29 20:58:18 +0800

    修复数据结构

--- model/cat.go
+++ model/cat.go
@@ -1,16 +1,22 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Cat struct {
-	gorm.Model
-	CatID             uint   `json:"cat_id" gorm:"type:integer;primaryKey;autoIncrement"`                       // 猫ID
-	CatName           string `json:"cat_name" gorm:"size:100;not null" binding:"required,min=1,max=100"`    // 猫名
-	CatPhotoUri       string `json:"cat_photo_uri" gorm:"size:1000;not null" binding:"required,url"`        // 猫照片
-	CatType           string `json:"cat_type" gorm:"size:100;not null" binding:"required,min=1,max=50"`     // 猫种类
-	CatGender         string `json:"cat_gender" gorm:"size:100;not null" binding:"required"`                // 猫性别
-	MasterName        string `json:"master_name" gorm:"size:100;not null" binding:"required,min=1,max=100"` // 主人姓名
-	MasterPhoneNumber string `json:"master_phone_number" gorm:"size:100;not null" binding:"required"` // 主人电话
+	CatID             uint `gorm:"primarykey"`
+	CreatedAt         time.Time
+	UpdatedAt         time.Time
+	DeletedAt         gorm.DeletedAt `gorm:"index"`
+	CatName           string         `json:"cat_name" gorm:"size:100;not null" binding:"required,min=1,max=100"`    // 猫名
+	CatPhotoUri       string         `json:"cat_photo_uri" gorm:"size:1000;not null" binding:"required,url"`        // 猫照片
+	CatType           string         `json:"cat_type" gorm:"size:100;not null" binding:"required,min=1,max=50"`     // 猫种类
+	CatGender         string         `json:"cat_gender" gorm:"size:100;not null" binding:"required"`                // 猫性别
+	MasterName        string         `json:"master_name" gorm:"size:100;not null" binding:"required,min=1,max=100"` // 主人姓名
+	MasterPhoneNumber string         `json:"master_phone_number" gorm:"size:100;not null" binding:"required"`       // 主人电话
 }
 
 type CatProfile struct {
--- model/site.go
+++ model/site.go
@@ -1,11 +1,17 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Site struct {
-	gorm.Model
-	SiteID               uint   `json:"site_id" gorm:"primaryKey"`                                              // 站点ID
-	SiteName             string `json:"site_name" gorm:"size:100;not null" binding:"required,min=1,max=100"`    // 站点名称
-	SiteAddress          string `json:"site_address" gorm:"size:100;not null" binding:"required,min=1,max=100"` // 站点地址
-	SiteAdminPhoneNumber string `json:"site_admin_phone_number" gorm:"size:100;not null" binding:"required"`    // 站点管理员电话
+	SiteID               uint `gorm:"primarykey"`
+	CreatedAt            time.Time
+	UpdatedAt            time.Time
+	DeletedAt            gorm.DeletedAt `gorm:"index"`
+	SiteName             string         `json:"site_name" gorm:"size:100;not null" binding:"required,min=1,max=100"`    // 站点名称
+	SiteAddress          string         `json:"site_address" gorm:"size:100;not null" binding:"required,min=1,max=100"` // 站点地址
+	SiteAdminPhoneNumber string         `json:"site_admin_phone_number" gorm:"size:100;not null" binding:"required"`    // 站点管理员电话
 }
