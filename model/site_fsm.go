15c1e28ecd7b61c63d892006cf9f49fb92475deb
NekokeCore <core2002@163.com>
2026-03-29 21:04:37 +0800

    修复状态机数据结构

--- model/site_fsm.go
+++ model/site_fsm.go
@@ -8,12 +8,14 @@ import (
 
 // SiteFSM 站点状态机信息
 type SiteFSM struct {
-	gorm.Model
-	SiteID            uint       `json:"site_id" gorm:"index;uniqueIndex;not null" binding:"required"` // 站点ID
-	LastDisinfectTime *time.Time `json:"last_disinfect_time" gorm:"type:datetime"`                     // 上次消毒时间
-	LastFeedTime      *time.Time `json:"last_feed_time" gorm:"type:datetime"`                          // 上次喂食时间
-	LastGiveWaterTime *time.Time `json:"last_give_water_time" gorm:"type:datetime"`                   // 上次喂水时间
-	LastPlayTime      *time.Time `json:"last_play_time" gorm:"type:datetime"`                         // 上次逗猫时间
+	SiteID            uint `gorm:"primarykey"`
+	CreatedAt         time.Time
+	UpdatedAt         time.Time
+	DeletedAt         gorm.DeletedAt `gorm:"index"`
+	LastDisinfectTime *time.Time     `json:"last_disinfect_time" gorm:"type:datetime"`  // 上次消毒时间
+	LastFeedTime      *time.Time     `json:"last_feed_time" gorm:"type:datetime"`       // 上次喂食时间
+	LastGiveWaterTime *time.Time     `json:"last_give_water_time" gorm:"type:datetime"` // 上次喂水时间
+	LastPlayTime      *time.Time     `json:"last_play_time" gorm:"type:datetime"`       // 上次逗猫时间
 }
 
 // TableName 指定表名
